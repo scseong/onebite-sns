9bee0aa16c8440903eb9841dfb9feb6ba33cc393
scseong <goidtv@naver.com>
2025-10-28 21:41:26 +0900

    Feat: 글로벌 레이아웃 설정

--- src/root-route.tsx
+++ src/root-route.tsx
@@ -8,20 +8,23 @@ import {
   SignInPage,
   SignUpPage,
 } from "./pages";
+import GlobalLayout from "./components/layout/global-layout";
 
 export default function RootRoute() {
   return (
     <Routes>
-      <Route path="/" element={<IndexPage />} />
-      <Route path="/sign-in" element={<SignInPage />} />
-      <Route path="/sign-up" element={<SignUpPage />} />
-      <Route path="/forget-password" element={<ForgetPasswordPage />} />
-      <Route path="/reset-password" element={<ResetPasswordPage />} />
+      <Route element={<GlobalLayout />}>
+        <Route path="/" element={<IndexPage />} />
+        <Route path="/sign-in" element={<SignInPage />} />
+        <Route path="/sign-up" element={<SignUpPage />} />
+        <Route path="/forget-password" element={<ForgetPasswordPage />} />
+        <Route path="/reset-password" element={<ResetPasswordPage />} />
 
-      <Route path="/post/:postId" element={<PostDetailPage />} />
-      <Route path="/profile/:userId" element={<ProfileDetailPage />} />
+        <Route path="/post/:postId" element={<PostDetailPage />} />
+        <Route path="/profile/:userId" element={<ProfileDetailPage />} />
 
-      <Route path="*" element={<Navigate to="/" />} />
+        <Route path="*" element={<Navigate to="/" />} />
+      </Route>
     </Routes>
   );
 }
