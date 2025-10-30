708a8bab3eb744e46400f5589a1f6ade64d0f11c
scseong <goidtv@naver.com>
2025-10-30 18:06:01 +0900

    Feat: 회원가입 시 프로필 정보 자동 생성

--- src/provider/session-provider.tsx
+++ src/provider/session-provider.tsx
@@ -1,11 +1,17 @@
 import { useEffect, type PropsWithChildren } from "react";
 import GlobalLoader from "@/components/global-loader";
-import { useIsSessionLoaded, useSetSession } from "@/store/session";
+import { useIsSessionLoaded, useSession, useSetSession } from "@/store/session";
+import useProfileData from "@/hooks/queries/use-profile-data";
 import supabase from "@/lib/supabase";
 
 export default function SessionProvider({ children }: PropsWithChildren) {
+  const session = useSession();
   const setSession = useSetSession();
   const isSessionLoaded = useIsSessionLoaded();
+
+  const { data: profile, isLoading: isProfileLoading } = useProfileData(
+    session?.user.id,
+  );
 
   useEffect(() => {
     supabase.auth.onAuthStateChange((event, session) => {
@@ -14,6 +20,7 @@ export default function SessionProvider({ children }: PropsWithChildren) {
   }, []);
 
   if (!isSessionLoaded) return <GlobalLoader />;
+  if (isProfileLoading) return <GlobalLoader />;
 
   return children;
 }
